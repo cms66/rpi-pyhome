c7e455a3a95c5e53ef74988aabe91ec73d36dced
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-08 18:55:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 # Initial setup
 # Download and run this file
-# 
+# https://raw.githubusercontent.com/cms66/rpi-pyhome/main/setup_base.sh
 print("test")
+# First boot - Base setup
+# Assumes
+# - rpi imager or sdm used to configure user/hostname
+# sudo run this script as created user
