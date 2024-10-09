40f372eb192e18447745a6636b4f7a3a4986721f
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-09 18:47:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import subprocess, sys
 
 # Variables
 cmdinp = sys.argv[1:]
-cmdscript = "lsblk"
+cmdscript = "sudo ls"
 
 # Run command
 # subprocess.run(command[0], shell = True, executable="/bin/bash")
