719918ed7967fe2fbff8af10ebc4b43862f8e1ed
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-09 19:47:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ print("Python setup") # works
 # subprocess.run(cmdinp[0], shell = True, executable="/bin/bash")
 # subprocess.run(cmdscript[0], shell = True, executable="/bin/bash")
 # print(subprocess.run(["fdisk", "-l"], capture_output=True)) # not a block device?
-print(subprocess.run(["sudo fdisk", "-l"], capture_output=True))
+print(subprocess.run(["sudo", "fdisk-l"], capture_output=True))
 print("Python setup done") # works
