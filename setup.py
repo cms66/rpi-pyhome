61a45c1499df00828a8d2483ca76b70d0ee3f194
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-09 18:15:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 # First boot - Base setup
 # Assumes
-# - rpi imager used to configure user/hostname
+# - rpi imager used to configure
+#    - user/password
+#    - hostname
+#    - WiFi
 # TODO
+# - Check for first run/delete on success
 
 # Imports
 import subprocess, sys
 
-# Set command to run
-#command = sys.argv[1:]
-#command = "sudo lsblk"
+# Variables
+cmdinp = sys.argv[1:]
+cmdscript = ""
 
 # Run command
 #subprocess.run(command[0], shell = True, executable="/bin/bash")
-#print("Python setup")
+print("Python setup")
 #print(subprocess.run(["echo", "Geeks for geeks"], capture_output=True))
 #print(subprocess.run(["sudo lsblk"], capture_output=True
 
