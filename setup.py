8aa8dd911774febfc4f67cdbad2fee79f4ec4df2
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-10 17:45:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ print("Python setup") # works
 # print(subprocess.run(["sudo", "fdisk", "-l"], capture_output=True)) # works
 #print(subprocess.run(["sudo", "fdisk", "-l"], capture_output=True)) # works
 #print(subprocess.run(["sudo", "sh", "test.sh"], shell = True, executable="/bin/bash"))
-print(subprocess.run(["sudo", "sh", "test.sh"], shell = True, executable="/bin/bash"))
+print(subprocess.run(["sh", "test.sh"], shell = True, executable="/bin/bash"))
 # print(subprocess.run(["sudo", "lsblk", "-l"], shell = True, executable="/bin/bash"))
 print("Python setup done") # works
