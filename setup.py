ac0406b063bb9dab5af158097079a62dfe6a4bdb
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-15 18:37:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,15 +17,14 @@ import subprocess, sys
 # Run commands
 print("Python setup") # works
 # subprocess.run(command[0], shell = True, executable="/bin/bash")
-print(subprocess.run(["echo", "Geeks for geeks"], capture_output=True)) # works
+# print(subprocess.run(["echo", "Geeks for geeks"], capture_output=True)) # works
 # subprocess.run(cmdinp[0], shell = True, executable="/bin/bash")
 # subprocess.run(cmdscript[0], shell = True, executable="/bin/bash")
 # print(subprocess.run(["fdisk", "-l"], capture_output=True)) # not a block device?
 # print(subprocess.run(["sudo", "fdisk", "-l"], capture_output=True)) # works
 # print(subprocess.run(["sudo", "fdisk", "-l"], capture_output=True)) # works
 # print(subprocess.run(["sudo", "sh", "test.sh"], shell = True, executable="/bin/bash"))
-#print(subprocess.run(["curl", "-L", "https://raw.githubusercontent.com/cms66/rpi-pyhome/main/setup_base.sh", "|", "sudo", "bash"], shell = True, executable="/usr/bin/bash"))
+# print(subprocess.run(["curl", "-L", "https://raw.githubusercontent.com/cms66/rpi-pyhome/main/setup_base.sh", "|", "sudo", "bash"], shell = True, executable="/usr/bin/bash"))
 print(subprocess.run(["curl", "-L", "https://raw.githubusercontent.com/cms66/rpi-pyhome/main/setup_base.sh", "|", "sudo", "bash"], capture_output=True))
-
 # print(subprocess.run(["sudo", "lsblk", "-l"], shell = True, executable="/bin/bash"))
 print("Python setup done") # works
