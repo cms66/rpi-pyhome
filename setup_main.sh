99600b5f235e26ed60d3a1cc627c7fbc36ea5084
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-20 18:15:26 +0100

    Create setup_main.sh

---
+++ setup_main.sh
@@ -0,0 +1,8 @@
+# Entry point for management system
+
+# Source shell scripts in same directory
+#for file in $(find $(dirname -- "$0") -type f -name "*.sh" ! -name $(basename "$0"));
+#do
+#source $file;
+#done
+
