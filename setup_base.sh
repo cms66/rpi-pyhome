b307c636a9a16b0675a2ec44543cdba620fba1a4
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-20 15:03:56 +0100

    Update setup_base.sh

--- setup_base.sh
+++ setup_base.sh
@@ -1,2 +1,2 @@
 echo "Bash setup"
-read -rp "Finished base setup press p to poweroff or any other key to reboot: " inp </dev/tty
+read -rp "Finished base setup press p to poweroff or any other key to reboot: " inp
