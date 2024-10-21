6eaf5f144dd88cd44c35c94b9af20d35c9c7c194
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-21 18:40:55 +0100

    Update setup_main.sh

--- setup_main.sh
+++ setup_main.sh
@@ -32,25 +32,6 @@ export repo
 reposcr=$PWD
 export reposcr
 
-show_system_summary()
-{
-	clear
-	#printf "System summary - $(hostname))\n--------------\n"
-	strtitle="System summary ($(hostname))"
-	echo $strtitle;printf -- '=%.0s' $(seq 1 ${#strtitle})
-	printf "\nRepo: $repo \n"
-	printf "\nRepo - script: $reposcr \n"
-	printf "\nModel: $pimodel \n"
-	printf "Revision: $pirev \n"
-	printf "Architecture: $osarch \n"
-	printf "Firmware: $(rpi-eeprom-update) \n"
-	printf "\nMemory:\n$pimem \n"
-	printf "\nStorage:\n$(lsblk) \n"
-	printf "Firewall "
-	ufw status
-	read -p "Press enter to return to menu" input
-}
-
 show_menu()
 {
 	printf "Main Menu\n"
