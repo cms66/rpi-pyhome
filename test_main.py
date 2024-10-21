4a865dbfee8874e0132f1f84c6e097ac9d44cfd0
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-21 16:07:34 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,1 +1,10 @@
+# Python based testing system
 
+# Imports
+import subprocess, sys
+
+# Run commands
+print("Test menu")
+#subprocess.run(["wget", "https://raw.githubusercontent.com/cms66/rpi-pyhome/main/base_setup.sh"])
+#print(subprocess.call(["sudo", "bash", "./base_setup.sh"]))
+print("Python testing done")
