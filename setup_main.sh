fe784b9b2f4826786443843b1c835c92257207b7
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-21 20:13:01 +0100

    Update setup_main.sh

--- setup_main.sh
+++ setup_main.sh
@@ -12,25 +12,15 @@ trap handle_error ERR
 
 # create variables for other functions
 usrname=$(logname)
-#export usrname
 usrpath="/home/$usrname"
-#export usrpath
 pinum=$(hostname | tr -cd '[:digit:].')
-#export pinum
 localnet=$(ip route | awk '/proto/ && !/default/ {print $1}')
-#export localnet
 pimodel=$(cat /sys/firmware/devicetree/base/model)
-#export pimodel
 pirev=$(cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//')
-#export pirev
 pimem=$(free -mt)
-#export pimem
 osarch=$(getconf LONG_BIT)
-#export osarch
 repo="rpi-pyhome"
-#export repo
-reposcr=$PWD
-#export reposcr
+dirscr=$PWD # Directory of calling script
 
 show_menu()
 {
