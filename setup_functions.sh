4e85367496128c9b6389015ad5606342c5e94fe6
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-21 18:28:36 +0100

    Create setup_functions.sh

---
+++ setup_functions.sh
@@ -0,0 +1,28 @@
+# Simple/generic functions
+
+show_system_summary()
+{
+	clear
+	strtitle="System summary ($(hostname))"
+	printf $strtitle;printf -- '=%.0s' $(seq 1 ${#strtitle})
+	printf "\nRepo: $repo \n"
+	printf "\nRepo - script: $reposcr \n"
+	printf "\nModel: $pimodel \n"
+	printf "Revision: $pirev \n"
+	printf "Architecture: $osarch \n"
+	printf "Firmware: $(rpi-eeprom-update) \n"
+	printf "\nMemory:\n$pimem \n"
+	printf "\nStorage:\n$(lsblk) \n"
+	printf "Firewall "
+	ufw status
+	read -p "Press enter to return to menu" input
+}
+
+# Pull git updates and return to working directory
+git_pull_setup()
+{
+    cd /home/$usrname/.pisetup/$repo
+    git pull https://github.com/cms66/$repo
+    cd $OLDPWD
+    read -p "Finished setup update, press enter to return to menu" input
+}
