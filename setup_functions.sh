f275b8353a07b9ec4fd192e98a4c721300d38521
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-23 16:37:42 +0100

    Update setup_functions.sh

--- setup_functions.sh
+++ setup_functions.sh
@@ -13,7 +13,7 @@ show_system_summary()
 	printf "Firmware: $(rpi-eeprom-update) \n"
 	printf "\nMemory:\n$pimem \n"
 	printf "\nStorage:\n$(lsblk) \n"
- 	#printf "\nDrive usage:\n$(df) \n"
+ 	printf "\nDrive usage:\n$(df -h) \n"
 	printf "Firewall "
 	ufw status
 	read -p "Press enter to return to menu" input
