7fbff0d3b7d06abb37e04e1c854e8844a20a3fd3
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-26 13:33:40 +0100

    Update setup_functions.sh

--- setup_functions.sh
+++ setup_functions.sh
@@ -48,7 +48,7 @@ setup_nfs_server()
 {
 	#apt-get -y install nfs-kernel-server
 	#ufw allow from $localnet to any port nfs
- 	check_package_status nfs-kernel-server
+ 	check_package_status nfs-kernel-server y
 	read -p "NFS Server setup done, press any key to return to menu" input
 }
 
