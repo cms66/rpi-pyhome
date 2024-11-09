bb6f91fa4ea77d5a6c3f2af3567f777e7c3efb36
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-09 17:04:36 +0000

    Update setup_functions.sh

--- setup_functions.sh
+++ setup_functions.sh
@@ -41,7 +41,7 @@ show_system_summary()
  	df -h
 	printf "\nFirewall "
 	ufw status
-	read -p "Press enter to return to menu" input; break;;
+	read -p "Press enter to return to menu" input; break 2;
 }
 
 # Pull git updates and return to working directory
