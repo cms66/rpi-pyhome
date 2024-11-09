1125c04353e99e80fa75e9ef6f839d87c9d3b283
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-09 17:30:59 +0000

    Update setup_functions.sh

--- setup_functions.sh
+++ setup_functions.sh
@@ -17,7 +17,7 @@ show_menu()
 			then
 		  		#read -p "you picked $menu ($REPLY) = Action: ${arrMenuActions[$menu]}"
 		  		${arrMenuActions[$menu]}
-      				printf "you picked $menu done"
+      				printf "$menu done"
 			else
 				read -p "Not a valid selection"
 			fi
