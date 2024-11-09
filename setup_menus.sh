ffdef1d4a4e5dc07e25983ffcbe62aef8f536d25
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-09 16:41:10 +0000

    Update setup_menus.sh

--- setup_menus.sh
+++ setup_menus.sh
@@ -1,15 +1,15 @@
 # Declare menus
 
-declare -A mnuMainActions;                               declare -a mnuMainPrompts;
-mnuMainActions["Hardware"]="setup_hardware";             mnuMainPrompts+=("Hardware");
-mnuMainActions["NFS"]="setup_nfs";                       mnuMainPrompts+=("NFS");
-mnuMainActions["OpenMPI"]="setup_openmpi";               mnuMainPrompts+=("OpenMPI");
-mnuMainActions["OpenCV"]="setup_opencv";                 mnuMainPrompts+=("OpenCV");
-mnuMainActions["SDM"]="setup_sdm";                       mnuMainPrompts+=("SDM");
-mnuMainActions["Update setup"]="git_pull_setup";         mnuMainPrompts+=("Update setup");
-mnuMainActions["Update system"]="update_system";         mnuMainPrompts+=("Update system");
-mnuMainActions["System summary"]="show_system_summary";  mnuMainPrompts+=("System summary");
-mnuMainActions["Quit"]="break 2";                        mnuMainPrompts+=("Quit");
+declare -A mnuMainActions;  declare -a mnuMainPrompts;
+mnuMainActions["Hardware"]="setup_hardware"; mnuMainPrompts+=("Hardware");
+mnuMainActions["NFS"]="setup_nfs"; mnuMainPrompts+=("NFS");
+mnuMainActions["OpenMPI"]="setup_openmpi"; mnuMainPrompts+=("OpenMPI");
+mnuMainActions["OpenCV"]="setup_opencv"; mnuMainPrompts+=("OpenCV");
+mnuMainActions["SDM"]="setup_sdm"; mnuMainPrompts+=("SDM");
+mnuMainActions["Update setup"]="git_pull_setup"; mnuMainPrompts+=("Update setup");
+mnuMainActions["Update system"]="update_system"; mnuMainPrompts+=("Update system");
+mnuMainActions["System summary"]="show_system_summary"; mnuMainPrompts+=("System summary");
+mnuMainActions["Quit"]="break 2"; mnuMainPrompts+=("Quit");
 
 declare -A mnuHardwareActions;      declare -a mnuHardwarePrompts;
 mnuHardwareActions["Camera - CSI"]="setup_camera_csi"; mnuHardwarePrompts+=("Camera - CSI");
