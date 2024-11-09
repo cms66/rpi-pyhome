2ba04dcd84e84140f37e3254bfa3dab826de8c09
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-09 16:48:43 +0000

    Update setup_menus.sh

--- setup_menus.sh
+++ setup_menus.sh
@@ -2,6 +2,9 @@
 
 declare -A mnuMainActions;  declare -a mnuMainPrompts;
 mnuMainActions["Hardware"]="setup_hardware"; mnuMainPrompts+=("Hardware");
+mnuMainActions["Update setup"]="git_pull_setup"; mnuMainPrompts+=("Update setup");
+mnuMainActions["Update system"]="update_system"; mnuMainPrompts+=("Update system");
+mnuMainActions["System summary"]="show_system_summary"; mnuMainPrompts+=("System summary");
 mnuMainActions["Quit"]="break 2"; mnuMainPrompts+=("Quit");
 
 declare -A mnuHardwareActions;      declare -a mnuHardwarePrompts;
