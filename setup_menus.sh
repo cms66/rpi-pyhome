d831bd971e4793f9b8be7880ed6926496330886f
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-09 16:16:48 +0000

    Update setup_menus.sh

--- setup_menus.sh
+++ setup_menus.sh
@@ -2,17 +2,16 @@
 
 declare -A mnuMainActions;      declare -a mnuMainPrompts;
 mnuMainActions["Hardware"]="setup_hardware"; mnuMainPrompts+=("Hardware");
-mnuMainActions["Opt 2"]="Act 2"; mnuMainPrompts+=("Opt 2");
-mnuMainActions["Opt 3"]="Act 3"; mnuMainPrompts+=("Opt 3");
-mnuMainActions["Opt 4"]="Act 4"; mnuMainPrompts+=("Opt 4");
-mnuMainActions["Opt 5"]="Act 5"; mnuMainPrompts+=("Opt 5");
-mnuMainActions["Opt 6"]="Act 6"; mnuMainPrompts+=("Opt 6");
+mnuMainActions["NFS"]="setup_nfs"; mnuMainPrompts+=("NFS");
+mnuMainActions["OpenMPI"]="setup_openmpi"; mnuMainPrompts+=("OpenMPI");
+mnuMainActions["OpenCV"]="setup_opencv"; mnuMainPrompts+=("OpenCV");
+mnuMainActions["SDM"]="setup_sdm"; mnuMainPrompts+=("SDM");
+mnuMainActions["Update setup"]="git_pull_setup"; mnuMainPrompts+=("Update setup");
+mnuMainActions["Update system"]="update_system"; mnuMainPrompts+=("Update system");
+mnuMainActions["System summary"]="show_system_summary"; mnuMainPrompts+=("System summary");
 mnuMainActions["Quit"]="break 2"; mnuMainPrompts+=("Quit");
 
 declare -A mnuHardwareActions;      declare -a mnuHardwarePrompts;
-mnuHardwareActions["Camera"]="setup_camera"; mnuHardwarePrompts+=("Camera");
-mnuHardwareActions["Opt 2"]="Act 2"; mnuHardwarePrompts+=("Opt 2");
-mnuHardwareActions["Opt 3"]="Act 3"; mnuHardwarePrompts+=("Opt 3");
-mnuHardwareActions["Opt 4"]="Act 4"; mnuHardwarePrompts+=("Opt 4");
-mnuHardwareActions["Opt 5"]="Act 5"; mnuHardwarePrompts+=("Opt 5");
+mnuHardwareActions["Camera - CSI"]="setup_camera_csi"; mnuHardwarePrompts+=("Camera - CSI");
+mnuHardwareActions["Camera - USB"]="setup_camera_usb"; mnuHardwarePrompts+=("Camera - USB");
 mnuHardwareActions["Back"]="break 2"; mnuHardwarePrompts+=("Back");
