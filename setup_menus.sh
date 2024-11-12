1b7e842abdbd6a3f2e002f08644914cb2d3e43d5
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-12 11:43:04 +0000

    Update setup_menus.sh

--- setup_menus.sh
+++ setup_menus.sh
@@ -1,13 +1,20 @@
 # Declare menus
 
-declare -A mnuMainActions;  declare -a mnuMainPrompts;
-mnuMainActions["Hardware"]="setup_hardware"; mnuMainPrompts+=("Hardware");
-mnuMainActions["Update setup"]="git_pull_setup"; mnuMainPrompts+=("Update setup");
-mnuMainActions["Update system"]="update_system"; mnuMainPrompts+=("Update system");
-mnuMainActions["System summary"]="show_system_summary"; mnuMainPrompts+=("System summary");
-mnuMainActions["Quit"]="break 2"; mnuMainPrompts+=("Quit");
+declare -a mnuMainFull=(
+"Hardware#setup_hardware"
+"Update setup#git_pull_setup"
+"Update system#update_system"
+"System summary#show_system_summary"
+"Quit#break 2"
+)
+#declare -A mnuMainActions;  declare -a mnuMainPrompts;
+#mnuMainActions["Hardware"]="setup_hardware"; mnuMainPrompts+=("Hardware");
+#mnuMainActions["Update setup"]="git_pull_setup"; mnuMainPrompts+=("Update setup");
+#mnuMainActions["Update system"]="update_system"; mnuMainPrompts+=("Update system");
+#mnuMainActions["System summary"]="show_system_summary"; mnuMainPrompts+=("System summary");
+#mnuMainActions["Quit"]="break 2"; mnuMainPrompts+=("Quit");
 
-declare -A mnuHardwareActions;      declare -a mnuHardwarePrompts;
-mnuHardwareActions["Camera - CSI"]="setup_camera_csi"; mnuHardwarePrompts+=("Camera - CSI");
-mnuHardwareActions["Camera - USB"]="setup_camera_usb"; mnuHardwarePrompts+=("Camera - USB");
-mnuHardwareActions["Back"]="break 2"; mnuHardwarePrompts+=("Back");
+#declare -A mnuHardwareActions;      declare -a mnuHardwarePrompts;
+#mnuHardwareActions["Camera - CSI"]="setup_camera_csi"; mnuHardwarePrompts+=("Camera - CSI");
+#mnuHardwareActions["Camera - USB"]="setup_camera_usb"; mnuHardwarePrompts+=("Camera - USB");
+#mnuHardwareActions["Back"]="break 2"; mnuHardwarePrompts+=("Back");
