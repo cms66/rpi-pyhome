f74f4f10fb8ffd73dcfedccc866f77e08f112342
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-12 18:09:45 +0000

    Update setup_menus.sh

--- setup_menus.sh
+++ setup_menus.sh
@@ -1,4 +1,4 @@
-# Declare menus - Brak option 0 will show at end of menu
+# Declare menus - Brake option 0 will show at end of menu
 #declare -a arrMenuPrompts # Empty array for menu prompts
 #declare -a arrMenuActions # Empty array for menu actions
 declare -a mnuMainFull=(
@@ -14,4 +14,9 @@ declare -a mnuHardwareFull=(
 "Camera - USB#setup_camera_usb"
 "Sense Hat#setup_sense_hat"
 )
-
+declare -a mnuNFSFull=(
+"Back#break 2"
+"Install server#install_nfs_server"
+"Add local export#add_nfs_local"
+"Add remote mount#add_nfs_remote"
+)
