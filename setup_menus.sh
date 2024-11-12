a4d6112325693b96b1df64101d777c2a6837b5bc
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-12 17:05:06 +0000

    Update setup_menus.sh

--- setup_menus.sh
+++ setup_menus.sh
@@ -1,6 +1,6 @@
 # Declare menus - Brak option 0 will show at end of menu
-declare -a arrMenuPrompts # Empty array for menu prompts
-declare -a arrMenuActions # Empty array for menu actions
+#declare -a arrMenuPrompts # Empty array for menu prompts
+#declare -a arrMenuActions # Empty array for menu actions
 declare -a mnuMainFull=(
 "Quit#break 2"
 "Hardware#setup_hardware"
