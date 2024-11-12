cf93fba66d4c2c8858cc5bf75cefa115de2df1e9
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-12 15:59:00 +0000

    Update setup_menus.sh

--- setup_menus.sh
+++ setup_menus.sh
@@ -1,15 +1,15 @@
-# Declare menus
+# Declare menus - Brak option 0 will show at end of menu
 
 declare -a mnuMainFull=(
+"Quit#break 2"
 "Hardware#setup_hardware"
 "Update setup#git_pull_setup"
 "Update system#update_system"
 "System summary#show_system_summary"
-"Quit#break 2"
 )
 declare -a mnuHardwareFull=(
+"Back#break 2"
 "Camera - CSI#setup_camera_csi"
 "Camera - USB#setup_camera_usb"
-"Back#break 2"
 )
 
