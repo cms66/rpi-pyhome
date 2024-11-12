6d2c644819a55c9763ae31ca0da79a42453082d8
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-12 16:32:32 +0000

    Update setup_hardware.sh

--- setup_hardware.sh
+++ setup_hardware.sh
@@ -14,6 +14,11 @@ setup_camera_usb()
 {
 	read -p "Function not yet available, press enter to continue"
 }
+
+setup_sense_hat()
+{
+	read -p "Function not yet available, press enter to continue"
+}
 update_firmware()
 {
 	if [ $pimodelnum = "4" ] || [ $pimodelnum = "5" ]; then # Model has firmware
