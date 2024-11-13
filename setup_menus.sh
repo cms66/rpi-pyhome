ce7d465b5005f1d5411d1b12eb2c3de18933cc61
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-13 13:45:31 +0000

    Update setup_menus.sh

--- setup_menus.sh
+++ setup_menus.sh
@@ -1,27 +1,46 @@
 # Declare menus - Break option 0 will show at end of menu
 
 declare -a mnuMainFull=(
-"Quit#break 2"
+"Setup - Main menu#"
 "Hardware#setup_hardware"
 "NFS#setup_nfs"
+"OpenMPI#setup_openmpi"
+"OpenCV#setup_opencv"
+"SDM#setup_sdm"
 "Update setup#git_pull_setup"
 "Update system#update_system"
 "System summary#show_system_summary"
+"Quit#break 2"
 )
 declare -a mnuHardwareFull=(
-"Back#break 2"
+"Setup - Hardware menu#"
 "Camera - CSI#setup_camera_csi"
 "Camera - USB#setup_camera_usb"
 "Sense Hat#setup_sense_hat"
+"Back#break 2"
 )
 declare -a mnuNFSFull=(
-"Back#break 2"
+"Setup - NFS menu#"
 "Install server#install_nfs_server"
 "Add local export#add_nfs_local"
 "Add remote mount#add_nfs_remote"
+"Back#break 2"
 )
 declare -a mnuOpenMPIFull=(
-"Back#break 2"
+"Setup - OpenMPI menu#"
 "Install - local#install_openmpi_local"
 "Install - server#install_server"
+"Back#break 2"
+)
+declare -a mnuOpenCVFull=(
+"Setup - OpenCV menu#"
+"Install - local#install_opencv_local"
+"Install - server#install_server"
+"Back#break 2"
+)
+declare -a mnuSDMFull=(
+"Setup - SDM menu#"
+"Install - local#install_sdm_local"
+"Install - server#install_server"
+"Back#break 2"
 )
