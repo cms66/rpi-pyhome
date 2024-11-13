b78f0ef171b9311588e6f1e4c675a5cde9ad245d
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-13 14:58:47 +0000

    Update setup_nfs.sh

--- setup_nfs.sh
+++ setup_nfs.sh
@@ -2,8 +2,26 @@
 
 install_nfs_server()
 {
-	#apt-get -y install nfs-kernel-server
 	#ufw allow from $localnet to any port nfs
  	check_package_status nfs-kernel-server y
 	#read -p "NFS Server setup done, press any key to return to menu" input
+}
+
+# Add local export
+add_nfs_local()
+{
+	read -p "NFS local export TODO, press enter to return to menu" input
+}
+
+# Add remote mount
+add_nfs_remote()
+{
+	read -p "Remote node (integer only): " nfsrem
+	read -p "Full path to remote directory (press enter for default = /var/nfs-export): " userdir
+	nfsdir=${userdir:="/var/nfs-export"}
+	mkdir $usrpath/share$nfsrem
+	chown $usrname:$usrname $usrpath/share$nfsrem
+	echo "pinode-$nfsrem.local:$nfsdir $usrpath/share$nfsrem    nfs defaults,user,exec,noauto,x-systemd.automount 0 0" >> /etc/fstab
+	mount -a
+	read -p "NFS remote mount done, press enter to return to menu" input
 }
