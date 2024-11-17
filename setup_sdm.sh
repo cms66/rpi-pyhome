38d8b8507e745a50e1c6b0339d09c9429fa31f94
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-17 19:53:50 +0000

    Update setup_sdm.sh

--- setup_sdm.sh
+++ setup_sdm.sh
@@ -1,1 +1,29 @@
 # SDM setup functions
+
+install_sdm_local()
+{
+    # Default setup - install to /usr/local/sdm
+	curl -L https://raw.githubusercontent.com/gitbls/sdm/master/EZsdmInstaller | bash
+  	# Create directories for images
+   	# Assumes
+    	# - NFS share already created
+     	# - 
+    
+  	read -rp "Path to image directory (press enter for default = $usrpath/share$pinum/sdm/images/): " userdir </dev/tty
+	$imgdir=${userdir:="$usrpath/share$pinum/sdm/images/"}
+ 	read -rp "WiFi country : " wfcountry </dev/tty
+ 	read -rp "WiFi SSID : " wfssid </dev/tty
+  	read -rp "WiFi Password : " wfpwd </dev/tty
+  	mkdir -p $imgdir/current
+  	mkdir -p $imgdir/latest
+   	mkdir -p $imgdir/archive
+    	chown -R $usrname:$usrname $imgdir
+ 	#download_latest_images
+  	# Create custom.conf in installation directory
+   	printf "# Custom configuration\n# --------------------\n\
+imgdirectory = $imgdir\n\
+wificountry = $wfcountry\n\
+wifissid = $wfssid\n\
+wifipassword = $wfpwd\n\
+# End of custom config\n" > $instdir/custom.conf
+}
