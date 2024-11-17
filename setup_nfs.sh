3b2a057c5b661192a0e0002d009a09ea2473213d
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-17 18:19:09 +0000

    Update setup_nfs.sh

--- setup_nfs.sh
+++ setup_nfs.sh
@@ -21,30 +21,46 @@ add_nfs_local()
     		install_nfs_server
      	fi
    	# Check mount type
-    read -p "System mount (default /usr/local) or Data share? (s/d) " inp
-    if [[ ${inp,} = "s" ]]
-    then # System mount (default /usr/local)
-    	defdir="/usr/local"
-    	read -p "Path of directory to be shared (press enter for default = $defdir): " userdir
+    	read -p "System mount (default /usr/local) or Data share? (s/d) " inp
+    	if [[ ${inp,} = "s" ]]
+    	then # System mount (default /usr/local)
+    		defdir="/usr/local"
+    		read -p "Path of directory to be shared (press enter for default = $defdir): " userdir
 		nfsdir=${userdir:="$defdir"}
 		# Check mount exists
-	 	if grep -F "$nfsdir" "/etc/exports"
-	 	then
-	  		read -p "export  exists"
-	  	else
+		if grep -F "$nfsdir" "/etc/exports"
+		then
+			read -p "export  exists"
+		else
 			echo "$nfsdir $localnet(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
-	  		exportfs -ra
-	  		read -p "export $nfsdir created"
+			exportfs -ra
+			read -p "export $nfsdir created"
 		fi
-    elif [[ ${inp,} = "d" ]]
+	elif [[ ${inp,} = "d" ]]
 	then # Data mount (default /var/), option to populate with standard content
 		defdir="/var/"    	
 		read -p "Path to directory for mounting share (press enter for default = $defdir): " userdir
 		mntdir=${userdir:="$defdir"}
  		# Data share path/name (default /home/username/share name)
+   		defname="share$pinum"
+   		read -p "Name of share (press enter for default = $defname): " usershare
+     		shrname=${usershare:="$defname"}
  		shrdir="$usrpath"
- 		read -p "Data share path (default /home/username/)"
- 		read -p "Data share name (default /home/username/share name)"
+   		read -p "Data share path (press enter for default = $shrdir): " dirshare
+     		shrpath=${dirshare:="$shrdir"}
+       		read -p "Populate with standard content? (y/n): " addcontent
+	 	if [[ ${addcontent,} = "y" ]]
+		then # extract nfs-export
+			tar -xvzf /home/$usrname/.pisetup/$repo/nfs-export.tgz -C $mntdir
+		else
+			mkdir $mntdir/nfs-export   			
+		fi
+  		chown -R $usrname:$usrname $mntdir/nfs-export/
+    		mkdir $usrpath/$shrname
+      		echo "$usrpath/$shrname $localnet(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
+		echo "$usrpath/$shrname    none	bind	0	0" >> /etc/fstab
+		exportfs -ra
+		mount -a
  		#read -p "NFS export added, press any key to return to menu" input
     else
     	read -p "invalid input"
