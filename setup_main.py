31d702b45f942fd066a1647127a2d5ec476fa2a3
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-18 21:22:44 +0000

    Update setup_main.py

--- setup_main.py
+++ setup_main.py
@@ -5,12 +5,8 @@ import subprocess, sys
 
 def main():
     print("Main setup")
-    
+    print (sys.argv)
 
 if __name__ == "__main__":
     main()
-# Run bash commands
-#print("Main setup")
-#subprocess.run(["wget", "https://raw.githubusercontent.com/cms66/rpi-pyhome/main/base_setup.sh"])
-#print(subprocess.call(["sudo", "bash", "./base_setup.sh"]))
-#print("Python setup done")
+
