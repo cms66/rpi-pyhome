00768ac579eaf61c3d073531e01f6e055d94bf47
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-23 16:31:51 +0000

    Update setup_sdm.sh

--- setup_sdm.sh
+++ setup_sdm.sh
@@ -97,6 +97,7 @@ modify_sdm_image()
  	readarray -t arrImg < <(find $imgdir/$dirlist -type f | awk -F "/" '{print $NF}')
   	#printf "Images\n-----\n"
 	#printf "Image: %s\n" "${arrImg[@]}"
+ 	clear
 	PS3="Select image: "
 	COLUMNS=1
 	select opt in "${arrImg[@]}" "Quit"
