066037bcbcfce7c560c0ff3e81e83fec37244d5c
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-28 13:35:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,11 +14,13 @@ import subprocess, sys
 # Run commands
 print("Python setup")
 subprocess.run(["wget", "https://raw.githubusercontent.com/cms66/rpi-pyhome/main/base_setup.sh"])
-subprocess.run(["sudo", "bash", "./base_setup.sh"])
+#subprocess.run(["sudo", "bash", "./base_setup.sh"])
 # TODO remove bash script
 subprocess.run(["sudo", "rm", "-f", "./base_setup.sh"])
-usropt=input("Python setup done, press p to poweroff or any other key to reboot: ").lower()
+usropt=input('Base seup done, press p to poweroff or any other key to reboot: ').lower()
 if usropt == 'p':
+   print('Poweroff')
    subprocess.run(["sudo", "poweroff"])
 else:
-   subprocess.run(["sudo", "reboot"])
+   print('Reboot')
+   subprocess.run(["sudo", "poweroff"])
