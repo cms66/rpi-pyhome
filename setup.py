f70654d88bb1e759909796da0c1d91c3435c713e
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-29 14:41:24 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,21 +6,28 @@
 #    - hostname
 
 # Imports
-import subprocess, sys, os
+import  subprocess, sys, os, urllib.request
 
-# Run commands
-print("Python setup")
-try:
-    subprocess.run(["wget", "https://raw.githubusercontent.com/cms66/rpi-pyhome/main/base_setup2.sh"])
-    #subprocess.run(["sudo", "bash", "./base_setup.sh"])
-    subprocess.run(["sudo", "rm", "-f", "./base_setup.sh"]) # Delete bash script
-    usropt=input("Base seup done, press p to poweroff or any other key to reboot: ").lower()
-    os.remove(__file__) # Delete python script
-    if usropt == 'p':
-        print ("Poweroff selected")
-        #subprocess.call(["shutdown", "-s", "now"])
-    else:
-        print ("Reboot selected")
-        #subprocess.call(["shutdown", "-r", "now"])
-except Exception as e:
-    print("The error is: ",e)
+def main():
+    bashfile = "base_setup.sh"
+    bashurl = "https://raw.githubusercontent.com/cms66/rpi-pyhome/main/" + bashfile
+    print("Python setup")
+    try:
+        urllib.request.urlretrieve(bashurl, bashfile) # Download bash script
+        subprocess.run(["sudo", "bash", "./" + bashfile], check=True) # Sudo run bash script
+        subprocess.run(["sudo", "rm", "-f", "./" + bashfile], check=True) # Delete bash script
+        usropt=input("Base seup done, press p to poweroff or any other key to reboot: ").lower()
+        os.remove(__file__) # Delete python script
+        if usropt == 'p':
+            print ("Poweroff selected")
+            subprocess.call(["shutdown", "-s", "now"])
+        else:
+            print ("Reboot selected")
+            subprocess.call(["shutdown", "-r", "now"])
+    except Exception as e:
+        print("Error:", e)
+        sys.exit(1)
+    print("Python setup done")
+    
+if __name__ == "__main__":
+    main()
