af12a50de58302ea4fbbe8838a3b64d39ed0e2ce
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-29 16:21:40 +0000

    Update setup_hardware.sh

--- setup_hardware.sh
+++ setup_hardware.sh
@@ -17,22 +17,16 @@ setup_sense_hat()
 
 update_firmware()
 {
-	if [[ $pimodelnum == "4" ]] # || [[ $pimodelnum == "5" ]]
- 	then # Model has firmware
+	if [[ $pimodelnum = "4" ]] || [ $pimodelnum = "5" ]; then # Model has firmware
 		printf "Model has firmware\n"
 		updfirm=$(sudo rpi-eeprom-update | grep BOOTLOADER | cut -d ":" -f 2 | tr -d '[:blank:]') # Check for updates
 		printf "Update status: $updfirm\n"
- 		if ! [[ $updfirm = "uptodate" ]]; then # Update available - TODO - test when updates are available
+ 		if ! [ $updfirm = "uptodate" ]; then # Update available - TODO - test when updates are available
  			printf "Update available\n"
-  			read -p "Firmware update available, press y to update now or any other key to continue: " input </dev/tty
-  			printf "Update selected\n"
-    			if [[ X$input = X"y" ]]; then # Apply firmware update
-    				printf "Update firmware\n"
-				rpi-eeprom-update -a
-   			fi
-    	 	else
-     			printf "Firmware is up to date\n"
-     		fi
+ 			rpi-eeprom-update -a
+    	 else
+     		printf "Firmware is up to date\n"
+     	fi
 	else
 		printf "No firmware\n"
 	fi
