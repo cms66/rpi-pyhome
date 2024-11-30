f10763420fbd87a05ea0ac5777a11684c0e6390e
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-30 13:55:08 +0000

    Update setup_sdm.sh

--- setup_sdm.sh
+++ setup_sdm.sh
@@ -111,13 +111,13 @@ modify_sdm_image()
   		case $img in
     		*.img)
 	 		read -p "Image selected"
-	 		break 2
+	 		;;
     		"Quit")
       			read -p "Quit selected"
-      			break 2
+      			;;
     		*)
       			read -p "Invalid option"
-      			break 2
+      			;;
   		esac
 	done
 }
