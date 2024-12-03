079f9cf5e7a82a88bb273c1dda9f9aabe9f48c0f
cms66 <58144229+cms66@users.noreply.github.com>
2024-12-03 10:13:45 +0000

    Update setup_openmpi.sh

--- setup_openmpi.sh
+++ setup_openmpi.sh
@@ -6,7 +6,7 @@ url32=https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.7.tar.gz
 url64=https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-5.0.6.tar.gz
 ver32="4.1.7"
 ver64="5.0.6"
-if [ "$osarch" = "64" ]
+if [[ "$osarch" = "64" ]]
 then
 	downlink=$url64
  	instver=$ver64
