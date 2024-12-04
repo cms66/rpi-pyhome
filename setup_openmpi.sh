2f3d509a0637c9e66665a0c4c36357b1b3db746e
cms66 <58144229+cms66@users.noreply.github.com>
2024-12-04 19:12:58 +0000

    Update setup_openmpi.sh

--- setup_openmpi.sh
+++ setup_openmpi.sh
@@ -30,3 +30,42 @@ install_openmpi_local()
 	mpirun --version
 	read -p "OpenMPI $instver - Local install finished, press enter to return to menu" input
 }
+
+install_openmpi_client()
+{
+	# TODO check server system folder (default /usr/local) is mounted
+ 	ldconfig
+  	read -p "OpenMPI $instver - Client install finished, press enter to return to menu" input
+}
+
+install_munge_local()
+{
+	# Create System group and user
+	groupadd -r -g 991 munge
+ 	useradd -r -g munge -u 991 -d /var/lib/munge -s /sbin/nologin munge
+  	# Install from Git
+   	git clone https://github.com/dun/munge.git
+	cd munge
+ 	./bootstrap
+  	./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --runstatedir=/run
+   	make
+    	make check
+	make install
+ 	# Security
+ 	chown munge:munge /etc/munge
+  	chmod 0700 /etc/munge
+ 	chown munge:munge /var/lib/munge
+  	chmod 0700 /var/lib/munge
+ 	chown munge:munge /var/log/munge
+  	chmod 0700 /var/log/munge
+ 	chown munge:munge /run/munge
+  	chmod 0755 /run/munge
+   	# Run munge at startup
+   	systemctl enable munge.service
+	# Create or copy key
+ 	#cp $usrpath/share1/munge.key /etc/munge/
+  	#$ sudo -u munge /usr/sbin/mungekey --verbose
+  	#chown munge:munge /etc/munge/munge.key
+	#sudo -u munge /usr/sbin/mungekey --verbose
+ 	read -p "Munge - Local install done, press enter to return to menu" input
+}
