e23e5eb4d5919b0885869bc471b4baf0cb5895ea
cms66 <58144229+cms66@users.noreply.github.com>
2024-12-04 11:56:54 +0000

    Update setup_opencv.sh

--- setup_opencv.sh
+++ setup_opencv.sh
@@ -1,1 +1,36 @@
 # OpenCV setup functions
+
+install_opencv_deps()
+{
+	apt-get -y install libjpeg-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libgtk2.0-dev libcanberra-gtk* libgtk-3-dev libgstreamer1.0-dev gstreamer1.0-gtk3 libgstreamer-plugins-base1.0-dev gstreamer1.0-gl libxvidcore-dev libx264-dev python3-numpy python3-pip libtbbmalloc2 libdc1394-dev libv4l-dev v4l-utils libopenblas-dev libatlas-base-dev libblas-dev liblapack-dev gfortran libhdf5-dev libprotobuf-dev libgoogle-glog-dev libgflags-dev protobuf-compiler
+}
+
+install_opencv_local()
+{
+	cd $usrpath
+	# Check memory
+	memtot=$(grep MemTotal /proc/meminfo | tr -cd '[:digit:].')
+ 	if [ $memtot -lt 5800000 ]
+	then
+		sed -i "s/CONF_SWAPSIZE=100/CONF_SWAPSIZE=2048/g" /etc/dphys-swapfile
+		/etc/init.d/dphys-swapfile restart
+	fi
+ 	install_deps
+	git clone https://github.com/opencv/opencv.git
+	git clone https://github.com/opencv/opencv_contrib.git
+	mkdir opencv/build
+	cd opencv/build
+	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=$usrpath/opencv_contrib/modules -D ENABLE_NEON=ON -D WITH_OPENMP=ON -D WITH_OPENCL=OFF -D BUILD_TIFF=ON -D WITH_FFMPEG=ON -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_GSTREAMER=ON -D BUILD_TESTS=OFF -D WITH_EIGEN=OFF -D WITH_V4L=ON -D WITH_LIBV4L=ON -D WITH_VTK=OFF -D WITH_QT=OFF -D WITH_PROTOBUF=ON -D OPENCV_ENABLE_NONFREE=ON -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D PYTHON3_PACKAGES_PATH=$usrpath/.venv/lib/python3.11/site-packages -D OPENCV_GENERATE_PKGCONFIG=ON -D BUILD_EXAMPLES=OFF ..
+	cores=$(nproc)
+	make -j$cores all
+	make install
+	ldconfig
+	cd $usrpath
+	rm -rf opencv*
+	if [ $memtot -lt 5800000 ]
+	then
+		sed -i "s/CONF_SWAPSIZE=2048/CONF_SWAPSIZE=100/g" /etc/dphys-swapfile
+		/etc/init.d/dphys-swapfile restart
+	fi
+	read -p "OpenCV Local install finished, press enter to return to menu" input
+}
