e06d3d53b86c01db83acc5fe3422d4a2c7f1a487
cms66 <58144229+cms66@users.noreply.github.com>
2024-12-12 14:50:18 +0000

    Update setup_ssh.sh

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -20,9 +20,10 @@ copy_user_ssh_keys()
 	read -p "Remote node: " remnode
 	read -p "copy to $usrname@$remnode, press enter to continue" # Confirms correct username/hostname
 	#runuser -u  $usrname -- "ssh-copy-id $usrname@$remnode"
-	#runuser -u  $usrname -- "ssh-copy-id -i $usrpath/.ssh/id_rsa.pub $usrname@$remnode"
+	runuser -u  $usrname -- 'ssh-copy-id -f -i $usrpath/.ssh/id_rsa.pub $usrname@$remnode'
 	#ssh-copy-id $usrname@$remnode
-	runuser -l $usrname -c 'sshpass –f $usrpath/.pisetup/pfile ssh-copy-id $usrname@$remnode'
+	#runuser -l $usrname -c 'sshpass –f $usrpath/.pisetup/pfile ssh-copy-id $usrname@$remnode'
 	#sudo -u $usrname -i bash -c 'ssh-copy-id -i /home/multipi/.ssh/id_rsa.pub $usrname@$remnode'
+ 	#ssh-copy-id -f -i hostkey.rsa.pub $usrname@$remnode
 	read -p "Pub key shared with $remnode, press enter to continue"
 }
